74809e2b0619d0fd98a5c5f48e3662ee60e37138
anv296 <anv296@gmail.com>
2018-08-10 11:29:13 +0700

    A13

--- main.cpp
+++ main.cpp
@@ -12,40 +12,21 @@ std::stack <char> brackets;
 
 bool check_brckts(char x){
       for( int j=0 ; j < strlen(closed) ; j++){
-          if (x[i]==opened[j]){
+          if (x==opened[j]){
               return true;
           }
-          else if (x[i]==closed[j]){
+          else if (x==closed[j]){
               return false;
           }
       }
 }
 
-/*bool check_o(char x){
-    for(int i=0 ; i<strlen(opened) ; i++){
-       if (x==opened[i]){
-           ::balance++;
-           return true;
-       }
-    }
-}
-
-bool check_c(char x){
-    for(int i=0 ; i<strlen(closed) ; i++){
-       if (x==closed[i]){
-           ::balance--;
-           return false;
-       }
-    }
-    return
-}*/
-
 bool check(char x , char k){
     for ( int i=0 ; i<strlen(opened) ; i++){
-        if (x==opened[j])
-            o=j;
-        else if (k==closed[j])
-            c=j;
+        if (x==opened[i])
+            o=i;
+        else if (k==closed[i])
+            c=i;
     }
     if (o == c){
         brackets.pop();
@@ -57,27 +38,16 @@ bool check(char x , char k){
 
 bool view(const char* string){
     for(int i=0 ; i<strlen(string) ; i++){
-
-        /*if (check_c(string[i])){
-            if (balance==-1)
-                return false;
-            else if (check(brackets, string[i]))
-        }
-        else if (check_o(string[i]) and (balance > 0)){
-            brackets.push(string[i]);
-        }
-        else if (check_c(string[i])){
-            if (check(brackets, string[i]))
-        }*/
-
         if (check_brckts(string[i]))
             brackets.push(string[i]);
-
         else if (check_brckts(string[i])){
-            if(check(brackets.top(), string[i])){
-
-            }
+            if(check(brackets.top(), string[i]))
+                return true;
+            else
+                return false;
         }
+        else
+            return true;
     }
     return true;
 }
@@ -86,10 +56,11 @@ int main(){
     std::string string;
     std::getline(std::cin, string);
 
-    if (view(string.c_str()))
+    if (view(string.c_str()) and (brackets.empty()))
         std::cout << "True" << std::endl;
     else
         std::cout << "False" << std::endl;
+
     system("pause");
     return 0;
 }
