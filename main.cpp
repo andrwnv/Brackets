8fcca574d60ff5713e6e3b25011aac0b495c554b
anv296 <anv296@gmail.com>
2018-08-10 09:04:21 +0700

    A10

--- main.cpp
+++ main.cpp
@@ -3,36 +3,40 @@
 #include <cstring>
 #include <cstdlib>
 
-bool check_o(char x){
-    char opened[] = "{[(";
+#include<stack>
 
+int balance(0);
+char opened[] = "{[(";
+char closed[] = "}})";
+
+bool check_o(char x){
     for(int i=0 ; i<strlen(opened) ; i++){
        if (x==opened[i]){
+           ::balance++;
            return true;
        }
     }
 }
 
 bool check_c(char x){
-    char closed[] = "}})";
-
     for(int i=0 ; i<strlen(closed) ; i++){
        if (x==closed[i]){
+           ::balance--;
            return false;
        }
     }
 }
 
 bool view(char x[]){
-    int balance = 0;
-
+    stack <char> brackets;
     for(int i=0 ; i<strlen(x) ; i++){
-        if (check_o(x[i]))
-            balance++;
-        else if (check_c(x[i]) && balance>0)
-            balance--;
-        else
-            return false;
+        if (check_c(x[i])){
+            if (balance==-1)
+                return false;
+        }
+        else if (check_o(x[i])){
+            brackets.push(x[i]);
+        }
     }
 }
 
